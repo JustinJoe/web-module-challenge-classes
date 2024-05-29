df64e370c4fe18fa001473b900e1dda2137b2666
Joseph Justin Opoku Mensah <josephjustin.om@gmail.com>
2024-05-29 22:47:43 +0000

    Completes Task 3

--- index.js
+++ index.js
@@ -119,7 +119,15 @@ class Car {
 */
 
 class Lambdasian {
+  constructor(obj) {
+    this.name = obj.name;
+    this.age = obj.age;
+    this.location = obj.location;
+  }
   
+  speak() {
+    return `Hello my name is ${this.name}. I am from ${this.location}`;
+  }
 }
 
 /*
