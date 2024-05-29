82c2dd4e67cf9b590ef5be0caaabb28dbfc473f0
Joseph Justin Opoku Mensah <josephjustin.om@gmail.com>
2024-05-29 22:45:19 +0000

    Completes Task 2

--- index.js
+++ index.js
@@ -80,7 +80,29 @@ class Person {
 */
 
 class Car {
-  
+  constructor(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+
+  fill(gallons) {
+    this.tank += gallons;
+  }
+
+  drive(distance) {
+    const tank = this.tank - distance / this.milesPerGallon;
+    
+    if (tank <= 0) {
+      this.odometer += this.milesPerGallon * this.tank;
+      this.tank = 0
+      return `I ran out of fuel at ${this.odometer} miles!`;
+    } else {
+      this.odometer += distance;
+      this.tank -= distance / this.milesPerGallon;
+    }
+  }
 }
 
 /*
